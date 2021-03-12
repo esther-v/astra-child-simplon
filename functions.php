88530aa3adf84b5c42897d8d82f6a61e6fc231ec
Esther V <esvalas@gmail.com>
2021-03-12 11:36:17 +0100

    add color palette

--- functions.php
+++ functions.php
@@ -73,3 +73,42 @@ function astra_child_simplon_add_body_class( $classes ) {
 
 // add custom color palette to the block editor
 
+add_theme_support( 'editor-color-palette', array(
+	array(
+		'name'  => __( 'White', 'astra-child-simplon' ),
+		'slug'  => 'white',
+		'color'	=> '#FFFFFF',
+	),
+  array(
+		'name'	=> __( 'Black', 'astra-child-simplon' ),
+		'slug'	=> 'black',
+		'color'	=> '#000000',
+	),
+	array(
+		'name'  => __( 'Carbon', 'astra-child-simplon' ),
+		'slug'  => 'carbon',
+		'color'	=> '#4a4a4a',
+	),
+	array(
+		'name'  => __( 'Caramel', 'astra-child-simplon' ),
+		'slug'  => 'caramel',
+		'color' => '#8B572A',
+	),
+	array(
+		'name'  => __( 'Vanilla', 'astra-child-simplon' ),
+		'slug'  => 'vanilla',
+		'color' => '#EBE5D9',
+	),
+	array(
+		'name'	=> __( 'Cappuccino', 'astra-child-simplon' ),
+		'slug'	=> 'cappuccino',
+		'color'	=> '#B09F82',
+	)
+	
+) );
+
+// function astra_child_simplon_add_theme_support() {
+	
+// }
+
+?>
